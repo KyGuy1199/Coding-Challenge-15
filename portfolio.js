6c1e65793ad9197273c4c8d3cbe103552298dc1f
Kyle Thomas <kylethomas1@usf.edu>
2024-11-07 18:15:57 -0500

    Create portfolio module with calculatePortfolioValue and getPortfolioAllocation functions

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+//Task 2: Create the Portfolio Module
+
+import {assets} from "./asset.js";
+
+export function calculatePortfolioValue() {
+    return assets.reduce((total, asset) => total + (asset.price * asset.quantity), 0);
+}       //funtion for calculating total portfolio value
+
+export function getPortfolioAllocation() {
+    const totalValue = calculatePortfolioValue();
+    return assets.map(assets => ([
+        name: asset.name,
+        allocation: ((asset.price * asset.quantity) / totalValue * 100).toFixed(2) +"%"
+    ]));
+}
+
